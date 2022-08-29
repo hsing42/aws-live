83c78840933d48aef25327b91cbfdb6317f2faac
hsing42 <99385080+hsing42@users.noreply.github.com>
2022-08-29 15:43:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.crtqicsjgoxm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "foozhihsing-employee"
+customregion = "us-east-1"
